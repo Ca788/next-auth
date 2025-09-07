9c35bcb31216d06e89682f63d89256c9b05a1092
Ca788 <carlos0ficial500@gmail.com>
2025-09-07 18:06:03 -0300

    Feat: add sign-in form and Radix UI components.

---
+++ src/components/sign-in/form.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export const SignInForm = () => {
+  return (
+    <div className="flex flex-col gap-6">
+      <Card>
+        <CardHeader>
+          <CardTitle>Acesse sua conta</CardTitle>
+          <CardDescription>
+            Digite seu email abaixo para entrar na sua conta
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Senha</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Esqueci minha senha
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full cursor-pointer">
+                  Login
+                </Button>
+                <Button variant="outline" className="w-full cursor-pointer">
+                  Login com Google
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm flex justify-center gap-1">
+               Não possui uma conta?
+              <a href="#" className="underline underline-offset-4">
+                Cadastre-se
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
